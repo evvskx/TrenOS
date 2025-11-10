0ffa3508dad84978fddd74ba5a4b06748c43daa9
evvskx <evvskw@gmail.com>
2025-11-10 20:14:19 +0100

    Use $tempDir on powerplan

--- src/Modules/Scripts/ApplyCustomPowerplan.ps1
+++ src/Modules/Scripts/ApplyCustomPowerplan.ps1
@@ -1,6 +1,6 @@
 function ApplyCustomPowerplan {
     try {
-        $path="$env:TEMP\\plan.pow"
+        $path="$tempDir\\plan.pow"
         & curl.exe -LSs "https://raw.githubusercontent.com/evvskx/TrenOS/refs/heads/main/Assets/Powerplan/BOHRV2.pow" -o "$path"
         $guid=(powercfg -import $path | ForEach-Object { ($_ -match 'GUID:\\s+([a-f0-9-]+)') | Out-Null; $matches[1] })
         powercfg -setactive $guid
